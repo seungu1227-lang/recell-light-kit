6f7c0e5606c3c457e5f4f8a33933c23cc7688cb6
류승우 <ryuseung-u@lyuseung-uui-MacBookAir.local>
2026-06-03 16:05:33 +0900

    Update deployed website

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.RECELL_SITE_CONFIG = {
-  SURVEY_URL: "https://example.com/recell-survey",
+  SURVEY_URL: "https://form.naver.com/response/3_y5Y6WamdmIAXwQZT12zw",
   VIDEO_EMBED_URL: "https://www.youtube.com/embed/hFCizDnvwNw"
 };
